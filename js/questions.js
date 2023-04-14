b8a2cac3ecfc2512b00460c8d5cac8e601538219
PRINCE ELLOH <prince.elloh@gmail.com>
2023-04-14 05:54:06 +0000

    First Commit
    First Date: 13-04-2023

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+
+
+
+let questions = [{
+  numb: 1,
+  question: "What is the capital city of France?",
+  answer: "Paris", options: [
+    "London", 
+    "Berlin", 
+    "Paris",
+    "Rome"
+  ]
+},
+{
+  numb: 2,
+  question: "How many sides does a triangle have?",
+  answer: "Three",
+  options: [
+    "One",
+    "Two",
+    "Three",
+    "Four"
+  ]
+},
+{
+  numb: 3,
+  question: "What is the color of the sun?",
+  answer: "Yellow",
+  options: [
+    "Blue",
+    "Green",
+    "Yellow",
+    "Red"
+  ]
+},
+{
+  numb: 4,
+  question: "What is the largest planet in our solar system?",
+  answer: "Jupiter",
+  options: [
+    "Earth",
+    "Saturn",
+    "Jupiter",
+    "Neptune"
+  ]
+},
+{
+  numb: 5,
+  question: "What is the name of the natural satellite of Earth?",
+  answer: "Moon",
+  options: [
+    "Mars",
+    "Venus",
+    "Moon",
+    "Mercury"
+  ]
+},
+{
+  numb: 6,
+  question: "What is the chemical symbol for water?",
+  answer: "H2O",
+  options: [
+    "CO2",
+    "H2O",
+    "NaCl",
+    "O2"
+  ]
+},
+{
+  numb: 7,
+  question: "What is the primary language spoken in Brazil?",
+  answer: "Portuguese",
+  options: [
+    "Spanish",
+    "Portuguese",
+    "English",
+    "French"
+  ]
+},
+{
+  numb: 8,
+  question: "What is the tallest mammal on Earth?",
+  answer: "Giraffe",
+  options: [
+    "Elephant",
+    "Rhinoceros",
+    "Giraffe",
+    "Hippopotamus"
+  ]
+},
+{
+  numb: 9,
+  question: "How many continents are there on Earth?",
+  answer: "Seven",
+  options: [
+    "Five",
+    "Six",
+    "Seven",
+    "Eight"
+  ]
+},
+{
+  numb: 10,
+  question: "Which programming construct allows you to repeat a block of code in Python?",
+  answer: "loop",
+  options: [
+    "if statement",
+    "function",
+    "loop",
+    "class"
+  ]
+}
+];
